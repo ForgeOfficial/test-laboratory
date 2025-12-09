4e73b94f4491761caaab871d105508a74f46a4fb
Maxime <maxime.caville@next-u.fr>
2025-12-09 10:55:22 +0100

    feat(green): throw if create laboratory without substances

--- src/laboratory.ts
+++ src/laboratory.ts
@@ -1,6 +1,7 @@
 export class Laboratory {
     private readonly substances: Map<string, number> = new Map();
     constructor(...substances: string[]) {
+        if (substances.length === 0) throw new Error('Laboratory need substances.');
         substances.forEach((substance) => this.substances.set(substance, 0));
     }
 
