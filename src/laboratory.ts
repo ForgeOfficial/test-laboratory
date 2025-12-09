77cb008417803bd14f3c53f711c960b9894d1cf7
Maxime <maxime.caville@next-u.fr>
2025-12-09 13:53:19 +0100

    feat(green): throw if reactions is invalid

--- src/laboratory.ts
+++ src/laboratory.ts
@@ -11,19 +11,20 @@ export class Laboratory {
         if (substances.length !== new Set(substances).size) throw new Error('Laboratory not accept duplicate substances.')
         substances.forEach((substance) => this.substances.set(substance, 0));
 
-        if (reactions)
-            Object.keys(reactions).forEach((reactionName) => {
-                const reaction = reactions[reactionName];
-                if (this.substances.has(reactionName)) throw new Error('Laboratory not accept reaction with same name has substances.');
-                reaction.forEach(element => {
-                    if (
-                        typeof element.quantity !== 'number' || element.quantity <= 0 ||
-                        typeof element.substance !== 'string'
-                    ) throw new Error('Laboratory need valid reactions.');
-                    if (!this.substances.has(element.substance)) throw new Error('Laboratory need reactions with valid substance.');
-                });
-                this.products.set(reactionName, 0);
-            })
+        if (!reactions || typeof reactions !== 'object' || Array.isArray(reactions)) throw new Error('The type of reactions is invalid.');
+
+        Object.keys(reactions).forEach((reactionName) => {
+            const reaction = reactions[reactionName];
+            if (this.substances.has(reactionName)) throw new Error('Laboratory not accept reaction with same name has substances.');
+            reaction.forEach(element => {
+                if (
+                    typeof element.quantity !== 'number' || element.quantity <= 0 ||
+                    typeof element.substance !== 'string'
+                ) throw new Error('Laboratory need valid reactions.');
+                if (!this.substances.has(element.substance)) throw new Error('Laboratory need reactions with valid substance.');
+            });
+            this.products.set(reactionName, 0);
+        })
     }
 
     getQuantity(element: string): number {
@@ -37,7 +38,7 @@ export class Laboratory {
     add(element: string, quantity: number): void {
         if (typeof element !== 'string') throw new Error('Element must be a string.');
         if (quantity <= 0 || typeof quantity !== 'number') throw new Error('Quantity must be a number and upper than 0');
-        const newQuantity = this.getQuantity(element)+quantity;
+        const newQuantity = this.getQuantity(element) + quantity;
         if (this.products.has(element))
             this.products.set(element, newQuantity);
         else this.substances.set(element, newQuantity);
