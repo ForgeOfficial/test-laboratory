1545332598e53f7cc9c63d18bba91e16ad2c168c
Maxime <maxime.caville@next-u.fr>
2025-12-09 10:59:02 +0100

    feat(green): throw if create laboratory with duplicate substances

--- src/laboratory.ts
+++ src/laboratory.ts
@@ -2,6 +2,7 @@ export class Laboratory {
     private readonly substances: Map<string, number> = new Map();
     constructor(...substances: string[]) {
         if (substances.length === 0) throw new Error('Laboratory need substances.');
+        if (substances.length !== new Set(substances).size) throw new Error('Laboratory not accept duplicate substances.')
         substances.forEach((substance) => this.substances.set(substance, 0));
     }
 
